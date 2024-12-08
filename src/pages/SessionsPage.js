709d62d75d29de59a9aadfef84dbecf04e3090f1
husam <husam@mail.com>
2024-12-08 09:04:27 +0100

    edited sessionPage

--- src/pages/SessionsPage.js
+++ src/pages/SessionsPage.js
@@ -1,18 +1,36 @@
+
 import React from 'react';
+
 import useFetchData from '../hooks/useFetchData';
-import SessionList from '../components/Session/SessionList';
+
+import SessionList from '../components/Conference/SessionList';
+
+
 
 const SessionsPage = ({ conferenceId }) => {
+
   const { data: sessions, loading } = useFetchData(
+
     `conferences/${conferenceId}/sessions`
+
   );
+
+
 
   return (
-    <div>
-      <h1>Sessions</h1>
-      {loading ? <p>Loading...</p> : <SessionList sessions={sessions} />}
-    </div>
-  );
-};
 
+    <div>
+
+      <h1>Sessions</h1>
+
+      {loading ? <p>Loading...</p> : <SessionList sessions={sessions} />}
+
+
+    </div>
+
+
+  
+
+)};
 export default SessionsPage;
+
